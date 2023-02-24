12163cc1231fc46c33f1f9cb31b62078ce9b38ab
Miguel Baños <info.miguelbanos@gmail.com>
2023-02-25 00:15:43 +0100

    add score, add tie +  dry code

--- src/Components/Square.jsx
+++ src/Components/Square.jsx
@@ -1,10 +1,7 @@
-
-// 4. Creamos las casillas (componente)
 const Square = ({ children, isSelected, updateBoard, index }) => {
-    /* 8. Genero un buleano de estilos para pintar las 'X' o las 'O' */
+
     const className = `square ${isSelected ? 'is-selected' : ''}`
   
-    /* 11.  Creo el 'handleclick' para cuando pinche el jugador actualice el estado */
     const handleClick = () => {
         updateBoard(index)
     }
